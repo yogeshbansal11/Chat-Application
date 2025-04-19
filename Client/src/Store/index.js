07af1d7f26e9a3f3ac2f71a663defed8d24941ca
yogesh bansal <bansalindia850@gmail.com>
2025-04-19 13:16:05 +0530

    updated slice

--- Client/src/Store/index.js
+++ Client/src/Store/index.js
@@ -1,15 +1,10 @@
-// import { createAuthSlice } from "./slices/auth-slice";
-// import { create } from "zustand";
-
-
-// export const useAppStore = create()((...a)=>({
-//   ...createAuthSlice(...a),
-// }))
 
 
 import { createAuthSlice } from "./slices/auth-slice";
 import { create } from "zustand";
+import { createChatSlice } from "./slices/chat-slice";
 
 export const useAppStore = create((...a) => ({
   ...createAuthSlice(...a),
+  ...createChatSlice(...a),
 }));
--- Client/src/Store/slices/auth-slice.js
+++ Client/src/Store/slices/auth-slice.js
@@ -1,7 +1,3 @@
-// export const createAuthSlice = (set) => ({
-//   userInfo: undefined,
-//   setUserInfo: (userInfo) => set({ userInfo }),
-// });
 
 
 
