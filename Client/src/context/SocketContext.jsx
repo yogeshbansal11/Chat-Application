74115cd72abdfe37982ac9ee5248defa33069a10
yogesh bansal <bansalindia850@gmail.com>
2025-05-04 20:33:14 +0530

    updated sockets

--- Client/src/context/SocketContext.jsx
+++ Client/src/context/SocketContext.jsx
@@ -25,7 +25,7 @@ export const SocketProvider = ({ children }) => {
       });
 
       const handleReceiveMessage = (message) => {
-        const { selectedChatData, selectedChatType, addMessage } =
+        const { selectedChatData, selectedChatType, addMessage, addContactsInDMContacts } =
           useAppStore.getState();
 
         if (
@@ -33,24 +33,28 @@ export const SocketProvider = ({ children }) => {
           (selectedChatData._id === message.sender._id ||
             selectedChatData._id === message.recipient._id)
         ) {
-          console.log("message received",message)
+          console.log("message received", message);
           addMessage(message);
         }
+        addContactsInDMContacts(message)
       };
 
-
-
       const handleReceiveChannelMessage = (message) => {
-        const { selectedChatData, selectedChatType, addMessage } =
-          useAppStore.getState();
+        const {
+          selectedChatData,
+          selectedChatType,
+          addMessage,
+          addChannelInChannelList,
+        } = useAppStore.getState();
 
-          if (
-            selectedChatType !== undefined &&
-            selectedChatData._id === message.channelId
-          ) {
-            addMessage(message);
-          }
-      }
+        if (
+          selectedChatType !== undefined &&
+          selectedChatData._id === message.channelId
+        ) {
+          addMessage(message);
+        }
+        addChannelInChannelList(message);
+      };
 
       socket.current.on("receiveMessage", handleReceiveMessage);
       socket.current.on("receive-channel-message", handleReceiveChannelMessage);
